810e001812778ab8932d018c52afe0034f619d09
민수아 <Minsua0503@gmail.com>
2024-09-07 14:42:29 +0900

    chore :: JWT 주석추가

--- src/main/java/org/example/be_sua/global/security/jwt/JwtTokenFilter.java
+++ src/main/java/org/example/be_sua/global/security/jwt/JwtTokenFilter.java
@@ -12,8 +12,9 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@RequiredArgsConstructor
-@Component
+@RequiredArgsConstructor // final이 붙은 필드 생성자 자동 생성
+@Component // 빈으로 등록
+// 매 요청마다 JWT 토큰을 검사하고 인증을 수행, 주로 JWT 기반 인증을 처리하는 데 사용함
 public class JwtTokenFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
@@ -22,10 +23,10 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
         throws ServletException, IOException {
-        String parseToken = jwtTokenProvider.resolveToken(request);
+        String parseToken = jwtTokenProvider.resolveToken(request); // 요청에서 JWT 토큰을 추출
         if (parseToken != null) {
-            Authentication authentication = jwtTokenProvider.authentication(parseToken);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            Authentication authentication = jwtTokenProvider.authentication(parseToken); // JWT 토큰을 기반으로 Authentication 객체를 생성
+            SecurityContextHolder.getContext().setAuthentication(authentication); // SecurityContextHolder에 인증 정보를 설정
         }
         filterChain.doFilter(request, response);
     }
--- src/main/java/org/example/be_sua/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/org/example/be_sua/global/security/jwt/JwtTokenProvider.java
@@ -17,18 +17,21 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-@RequiredArgsConstructor
-@Component
+@RequiredArgsConstructor // final이 붙은 필드에 대한 생성자 자동 생성
+@Component // 이 클래스를 빈으로 등록
+// JWT 토큰을 생성하고 관리하는 클래스
 public class JwtTokenProvider {
 
     private final RefreshTokenRepository refreshTokenRepository;
     private final JwtProperty jwtProperty;
     private final AuthDetailsService authDetailsService;
 
+    // 액세스 토큰 생성
     public String generateAccessToken(String accountId) {
         return generateToken(accountId, "access", jwtProperty.getAccessExp());
     }
 
+    // 리프레시 토큰 생성, 저장
     public String generateRefreshToken(String accountId) {
         String refreshToken = generateToken(accountId, "refresh", jwtProperty.getRefreshExp());
         refreshTokenRepository.save(RefreshToken.builder()
@@ -39,21 +42,24 @@ public class JwtTokenProvider {
         return refreshToken;
     }
 
+    // 공통적으로 사용하는 토큰 생성 메서드 : JWT의 서명 방식, 발급자, 만료 시간 등을 설정하여 토큰 생성
     public String generateToken(String accountId, String type, Long exp) {
         return Jwts.builder()
-                .signWith(SignatureAlgorithm.HS256, jwtProperty.getSecretKey())
-                .setSubject(accountId)
-                .claim("type", type)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + exp * 1000))
-                .compact();
+                .signWith(SignatureAlgorithm.HS256, jwtProperty.getSecretKey()) // 서명을 추가하는 부분
+                .setSubject(accountId) // 토큰의 주체를 설정
+                .claim("type", type) // 토큰 유형 저장
+                .setIssuedAt(new Date()) // 발급된 시간 설정
+                .setExpiration(new Date(System.currentTimeMillis() + exp * 1000)) // 만료 시간 설정
+                .compact(); // 설정된 정보를 바탕으로 JWT 문자열 생성하여 반환
     }
 
+    // HTTP 요청에서 JWT 토큰 추출 (해당 헤더에서 JWT를 가져와 토큰만 추출하여 반환)
     public String resolveToken(HttpServletRequest request) {
-        String bearer = request.getHeader(jwtProperty.getHeader());
-        return parseToken(bearer);
+        String bearer = request.getHeader(jwtProperty.getHeader()); //요청 해더에서 토큰 가져옴
+        return parseToken(bearer); // 토큰에서 실제 값을 파싱하여 반환
     }
 
+    // Authorization 헤더에서 "Bearer"를 제거하여 실제 JWT 토큰만 반환
     public String parseToken(String bearerToken) {
         if (bearerToken != null && bearerToken.startsWith(jwtProperty.getPrefix())) {
             return bearerToken.replace(jwtProperty.getPrefix(), "");
@@ -61,15 +67,18 @@ public class JwtTokenProvider {
         return null;
     }
 
+    // 토큰을 기반으로 Authentication 객체를 생성
     public Authentication authentication(String token) {
         UserDetails userDetails = authDetailsService.loadUserByUsername(getTokenSubject(token));
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
+    // 토큰에서 사용자 ID 추출
     public String getTokenSubject(String token) {
         return getTokenBody(token).getSubject();
     }
 
+    // 토큰의 클레임을 반환(JWT를 파싱하고, 유효성 검사를 수행) : 예외가 발생하면 적절한 예외 던짐
     public Claims getTokenBody(String token) {
         try {
             return Jwts.parser().setSigningKey(jwtProperty.getSecretKey())

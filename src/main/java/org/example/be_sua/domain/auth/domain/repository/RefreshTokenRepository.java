f8cebdacb47b8768f12cc90b31fd2ceba291f95a
민수아 <Minsua0503@gmail.com>
2024-09-08 19:11:50 +0900

    feat :: @Repository 어노테이션 추가

--- src/main/java/org/example/be_sua/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/org/example/be_sua/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -1,12 +1,12 @@
 package org.example.be_sua.domain.auth.domain.repository;
 
 import org.example.be_sua.domain.auth.domain.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
     Optional<RefreshToken> findRefreshToken(String refreshToken);
 }

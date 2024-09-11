510d870180fa1fcf7a9b04ded048b8b8dd7cb46a
민수아 <Minsua0503@gmail.com>
2024-09-11 18:15:25 +0900

    fix :: @RequestMapping 수정

--- src/main/java/org/example/be_sua/domain/user/presentation/UserController.java
+++ src/main/java/org/example/be_sua/domain/user/presentation/UserController.java
@@ -6,19 +6,18 @@ import org.example.be_sua.domain.user.presentation.dto.request.LoginRequest;
 import org.example.be_sua.domain.user.presentation.dto.request.SignUpRequest;
 import org.example.be_sua.domain.user.service.LoginService;
 import org.example.be_sua.domain.user.service.SignupService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
     private final SignupService signupService;
     private final LoginService loginService;
 
-    @PostMapping("/signup") // 회원가입
+    @PostMapping("/signup")
+    @ResponseStatus(HttpStatus.CREATED) // 회원가입
     public void signup(@RequestBody @Valid SignUpRequest signUpRequest) {
         signupService.signup(signUpRequest);
     }

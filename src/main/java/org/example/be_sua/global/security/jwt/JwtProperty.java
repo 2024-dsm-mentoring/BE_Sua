c981b0454daef9c69cc2ff0ca61797630823ea74
민수아 <Minsua0503@gmail.com>
2024-09-11 18:17:19 +0900

    fix :: prefix 수정

--- src/main/java/org/example/be_sua/global/security/jwt/JwtProperty.java
+++ src/main/java/org/example/be_sua/global/security/jwt/JwtProperty.java
@@ -3,10 +3,11 @@ package org.example.be_sua.global.security.jwt;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @Getter
 @AllArgsConstructor
-@ConfigurationProperties(prefix = "jwt")
+@ConfigurationProperties(prefix = "spring.jwt")
 public class JwtProperty {
 
     private final String secretKey;
